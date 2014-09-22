a3a59205254ce12560e1db4337f7bea04f6c4fbe
Benjamin Brown <ben.brown@factmint.com>
2014-09-22 14:30:51 +0100

    Variable name fixes

--- dist/scale-utils.js
+++ dist/scale-utils.js
@@ -9,8 +9,18 @@ function(NumberUtils) {
 				throw "Min value and max are the same";
 			}
 			var backwards = (minValue > maxValue) ? true : false;
-			this.getPixel = function(point) {
-				if (point < minValue || point > maxValue) {
+			this.getPixel = function(point, allowOutOfRangeValues) {
+				
+				if (typeof allowOutOfRangeValues === 'undefined') {
+					allowOutOfRangeValues = true;
+				} else {
+					allowOutOfRangeValues = false;
+				}
+
+				if (
+					(point < minValue || point > maxValue) &&
+					allowOutOfRangeValues === false
+				) {
 					if (! (backwards && point <= minValue && point >= maxValue)) {
 						throw "Out of range";
 					}
@@ -19,37 +29,92 @@ function(NumberUtils) {
 				return pixelOffset + pixelRange * proportion;
 			};
 		},
+		
+		/**
+		 * Generates and returns an array of tick values
+		 * Taken from Graphic Gems 1 - Nice Numbers for Graph Labels
+		 * @link: http://inis.jinr.ru/sl/vol1/CMC/Graphics_Gems_1,ed_A.Glassner.pdf
+		 * @link: https://github.com/erich666/GraphicsGems/blob/master/gems/Label.c
+		 * 
+		 * @param	{Number} minValue					
+		 * @param	{Number} maxValue					
+		 * @param	{Number} targetMarkerCount 
+		 * @return {Array}									 
+		 */
 		getTickMarks: function(minValue, maxValue, targetMarkerCount) {
+			
+			var decimalPlaces = 0;
+			var tickSpacing = 0;
+			var graphMin = 0;
+			var graphMax = 0;
+			var range = 0;
+			var value = 0;
 			var values = [];
-
-			var range = Math.abs(maxValue - minValue);
-			var orderlessTarget = (range >= 1 && range <= 30) ? 1 : 5;
-			var degrees = range < 1000 ? 1 : 2;
-
-			if (! stepSize) {
-				var stepSize = NumberUtils.roundToOrder(
-					range / targetMarkerCount,
-					orderlessTarget,
-					degrees
-				);
+			
+			range = getNiceNumber(maxValue - minValue, false);
+			tickSpacing = getNiceNumber(range / (targetMarkerCount - 1), true);
+			graphMin = Math.floor(minValue / tickSpacing) * tickSpacing;
+			graphMax = Math.ceil(maxValue / tickSpacing ) * tickSpacing;
+			decimalPlaces = Math.max(-Math.floor(log10(tickSpacing)), 1);
+			
+			for (value = graphMin; value < graphMax + 0.5 * tickSpacing; value += tickSpacing) {
+				values.push(parseFloat(value.toFixed(decimalPlaces)));
 			}
 
-			if (minValue < 0) {
-				for (var value = 0; value < maxValue + stepSize; value += stepSize) {
-					var newValue = value;
-					values.push(newValue);
-				}
-				for (var value = -stepSize; value > minValue - stepSize; value -= stepSize) {
-					var newValue = value;
-					values.splice(0, 0, newValue);
-				}
-			} else {
-				for (var value = minValue; value < maxValue + stepSize; value += stepSize) {
-					var newValue = value;
-					values.push(newValue);
-				}
-			}
 			return values;
+
+			/**
+			 * Returns the base 10 logarithm of a number
+			 * @param	{Number} value 
+			 * @return {Number}
+			 */
+			function log10(value) {
+				return Math.log(value) / Math.LN10;
+			}
+
+
+			/**
+			 * Returns a nice looking number
+			 * @param	{Number} value
+			 * @param	{Boolean} roundNumber
+			 * @return {Number}
+			 */
+			function getNiceNumber(value, roundNumber) {
+
+				var exponentValue = 0;
+				var fractionalValue = 0;
+				var niceRoundFraction = 0;
+
+				exponentValue = Math.floor(log10(value));
+				fractionalValue = value / Math.pow(10, exponentValue);
+
+				if (roundNumber) {
+					if (fractionalValue < 1.5) {
+						niceRoundFraction = 1;
+					} else if (fractionalValue < 3) {
+						niceRoundFraction = 2;
+					} else if (fractionalValue < 7) {
+						niceRoundFraction = 5;
+					} else {
+						niceRoundFraction = 10;
+					}
+				} else {
+					if (fractionalValue <= 1) {
+						niceRoundFraction = 1;
+					} else if (fractionalValue <= 2) {
+						niceRoundFraction = 2;
+					} else if (fractionalValue <= 5) {
+						niceRoundFraction = 5;
+					} else {
+						niceRoundFraction = 10;
+					}
+				}
+
+				return niceRoundFraction * Math.pow(10, exponentValue);
+
+			}
+
 		}
+		
 	}
 });
