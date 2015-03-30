3d0b60d299ab3dc5356b39ccb8ae1d08c9756ce5
Chris Scott <chris.scott@factmint.com>
2015-03-30 13:44:14 +0000

    Added code to handle equal min and max values

--- dist/scale-utils.js
+++ dist/scale-utils.js
@@ -7,7 +7,14 @@ define(['number-utils'],
 				this.end = pixelOffset + pixelRange;
 				
 				if (minValue == maxValue) {
-					throw "Min value and max are the same";
+					if (minValue === 0) {
+						minValue = 0;
+						maxValue = 1;
+					} else {
+						var difference = Math.abs(minValue * 1/10);
+						minValue = minValue - difference;
+						maxValue = maxValue + difference;
+					}
 				}
 				
 				var backwards = (minValue > maxValue) ? true : false;
